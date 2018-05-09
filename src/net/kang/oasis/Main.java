fd2473a2ef893f8679d367634d52f54b9e8b5ea3
tails5555 <tails5555@naver.com>
2018-05-10 02:51:14 +0900

    Update Oasis Problem 반대로 Queue를 이용해서 해 봤는데 내일 저녁에 한 번 고민을 해봐야 될 듯 합니다...

--- src/net/kang/oasis/Main.java
+++ src/net/kang/oasis/Main.java
@@ -1,36 +1,52 @@
 package net.kang.oasis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     static int N;
     static int[] heights;
-    static Scanner s = new Scanner(System.in);
-    public static void input(){
+    // static Scanner s = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void input() throws IOException {
+        /*
         N = s.nextInt();
         heights = new int[N];
         for(int k=0;k<N;k++){
             heights[k]=s.nextInt();
         }
+        */
+        N = Integer.parseInt(br.readLine());
+        heights = new int[N];
+        for(int k=0;k<N;k++){
+            heights[k]=Integer.parseInt(br.readLine());
+        }
     }
-    public static int solution(){
-        Stack<Integer> s = new Stack<Integer>();
-        int count = 0;
-        s.push(heights[0]);
-        for(int k=1;k<N;k++){
-            if(s.peek() > heights[k]){
-                s.pop();
-                s.push(heights[k]);
-
-            }else{
-                s.push(heights[k]);
+    public static long solution(){
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        long count = 0;
+        for(int k = 0; k < N-1; k++){
+            q.addLast(heights[k]);
+            for(int l = k+1; l<N; l++){
+                if(q.peekFirst() >= heights[l]){
+                    System.out.println(q);
+                    if(k == N-2) count+=1;
+                    q.addLast(heights[l]);
+                }else{
+                    System.out.println("NOT"+q);
+                    count += q.size();
+                    break;
+                }
             }
+            q.clear();
         }
         return count;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         input();
-        System.out.println();
+        System.out.println(solution());
     }
 }
