483fc531eaf478ad535395da6945f435d80c7648
tails5555 <tails5555@naver.com>
2018-05-26 10:35:47 +0900

    Create Word Math 백준 1339번 문제 풀이 완료

---
+++ src/net/kang/wordmath/Main.java
@@ -0,0 +1,31 @@
+package net.kang.wordmath;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 백준 1339번 문제 풀이 완료
+public class Main {
+    static int[] alphabet = new int[26];
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        for(int k=0;k<N;k++){
+            String tmp = s.next();
+            char[] chars = tmp.toCharArray();
+            for(int l=0;l<chars.length;l++){
+                alphabet[chars[l]-'A']+=(int)Math.pow(10, chars.length-1-l);
+            }
+        }
+        Arrays.sort(alphabet);
+
+        int ans = 0;
+        int value = 9;
+
+        for(int k=alphabet.length-1;k>=0;k--){
+            if(value!=0)
+                ans += value-- * alphabet[k];
+            else break;
+        }
+        System.out.println(ans);
+    }
+}
