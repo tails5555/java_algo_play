bec9d6fdf01efc46c4ed8caa6b96099834b41868
tails5555 <tails5555@naver.com>
2018-07-04 20:34:36 +0900

    Create Coin 1, Make One Problems DP 문제 간단한 실습 코딩 올림.

---
+++ src/net/kang/coin1/Main.java
@@ -0,0 +1,24 @@
+package net.kang.coin1;
+
+import java.util.Scanner;
+
+// 백준 2293번 문제 풀이
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int K = s.nextInt();
+        int[] dp = new int[10001];
+        int[] coins = new int[N];
+        for(int k=0;k<N;k++){
+            coins[k] = s.nextInt();
+        }
+        dp[0] = 1;
+        for(int k = 0; k < N; k++) {
+            for(int l = 1; l <= K; l++) {
+                if(l - coins[k] >= 0) dp[l] += dp[l - coins[k]];
+            }
+        }
+        System.out.println(dp[K]);
+    }
+}
