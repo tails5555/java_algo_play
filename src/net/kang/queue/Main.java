fc39b52ab9a01a14d91bb1a0f308ef17d74c33ed
tails5555 <tails5555@naver.com>
2018-08-28 21:28:22 +0900

    Create RoomNumber, Data Structure 기초 문제 풀이 완료. 요즘 알고리슴을 많이 안 해서 그런지 언넝 적응해야 됨...ㅜㅜ

---
+++ src/net/kang/queue/Main.java
@@ -0,0 +1,44 @@
+package net.kang.queue;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+// 백준 큐 기초 문제 풀이
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+        for(int k=0;k<N;k++){
+            String s = br.readLine();
+            if(s.startsWith("push ")){
+                queue.offer(Integer.parseInt(s.replace("push ", "")));
+            } else {
+                switch(s){
+                    case "front" :
+                        bw.append(String.format("%d\n", queue.isEmpty() ? -1 : queue.peekFirst()));
+                        break;
+                    case "back" :
+                        bw.append(String.format("%d\n", queue.isEmpty() ? -1 : queue.peekLast()));
+                        break;
+                    case "size" :
+                        bw.append(String.format("%d\n", queue.size()));
+                        break;
+                    case "pop" :
+                        bw.append(String.format("%d\n", queue.isEmpty() ? -1 : queue.removeFirst()));
+                        break;
+                    case "empty" :
+                        bw.append(String.format("%d\n", queue.isEmpty() ? 1 : 0));
+                        break;
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
