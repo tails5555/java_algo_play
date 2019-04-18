1d192f207344bea4f7ea9d09e7931bc736e2171b
tails5555 <tails5555@naver.com>
2019-04-18 15:38:56 +0900

    Init Baekjoon KOI 2 Questions KOI 2004 본선 문제 2문 풀이 완료.

---
+++ src/net/koi/Main2605.java
@@ -0,0 +1,44 @@
+package net.koi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+// Baekjoon 2605 번 KOI 2004 초등부 줄 세우기 문제 풀이
+public class Main2605 {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N;
+    static int[] order;
+
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        order = new int[N + 1];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int k = 1; k <= N; k++){
+            order[k] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void solution() {
+        LinkedList<Integer> students = new LinkedList<>();
+        students.add(1); // 1번 학생은 0번을 뽑을 수 밖에 없어 언제나 첫 바운드에 시작한다.
+        for(int k = 2; k <= N; k++){
+            if(order[k] == 0) students.addLast(k);
+            else students.add(k - order[k] - 1, k);
+        }
+        for(int k : students){
+            System.out.printf("%d ", k);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
---
+++ src/net/koi/Main2610.java
@@ -0,0 +1,98 @@
+package net.koi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// Baekjoon 2610 번 KOI 2004 중등부 3번 회의 준비 문제 풀이
+// 다행히도 N 은 작은 값(100) 이라서 플로이드 워샬 알고리즘이면 충분하다.
+public class Main2610 {
+    static final int MAX = 987654321;
+
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    static int N;
+    static int[][] froyd;
+
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        froyd = new int[N + 1][N + 1];
+        for(int k = 0; k < froyd.length; k++){
+            Arrays.fill(froyd[k], MAX);
+            froyd[k][k] = 0;
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        for(int k = 0; k < M; k++){
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            froyd[from][to] = froyd[to][from] = 1;
+        }
+    }
+
+    static void solution() {
+        for(int m = 1; m <= N; m++){
+            for(int a = 1; a <= N; a++){
+                for(int b = 1; b <= N; b++){
+                    froyd[a][b] = Math.min(froyd[a][b], froyd[a][m] + froyd[m][b]);
+                }
+            }
+        }
+
+        List<Integer> leader = new ArrayList<>();
+        boolean[] visited = new boolean[N + 1];
+        int[] sum = new int[N + 1];
+
+        int cnt = 0;
+        for(int k = 1; k <= N; k++){
+            if(!visited[k]) cnt++;
+            int max = 0;
+            for(int l = 1; l <= N; l++){
+                if(froyd[k][l] == MAX) continue;
+                visited[l] = true;
+                max = Math.max(froyd[k][l], max);
+            }
+            sum[k] = max;
+        }
+        Arrays.fill(visited, false);
+
+        for(int k = 1; k <= N; k++){
+            if(!visited[k]) {
+                visited[k] = true;
+                int min = MAX + 1;
+                int tmp = k;
+                for(int l = 1; l <= N; l++){
+                    if(k != l && froyd[k][l] != MAX){
+                        visited[l] = true;
+                        if(min > sum[l]){
+                            tmp = l;
+                            min = sum[l];
+                        }
+                    }
+                }
+                leader.add(tmp);
+            }
+        }
+
+        System.out.println(cnt);
+        Collections.sort(leader);
+        for(int k : leader){
+            System.out.println(k);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+}
